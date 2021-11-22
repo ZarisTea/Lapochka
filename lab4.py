b9e52e2ae3d6034fab63493d810f0fe40b55f820
Zaris <egor-grishenko@mail.ru>
2021-11-22 22:14:07 +0700

    Добавил СЕМЕНА

--- lab4.py
+++ lab4.py
@@ -7,7 +7,7 @@ class Cars():
         
     def add(self):
         vvod = ''
-        while vvod != "стоп":
+        while vvod != "SiemenOFF":
                 print("Введите цвет, объем двигателя и производителя автомобиля через пробел(если машин больше нет, пишите команду 'стоп':")
                 avt = input().split()
                 if avt != []:
